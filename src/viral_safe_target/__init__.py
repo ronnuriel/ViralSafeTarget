e696147754d7d3749910351427682e1b0965aecb
Ron Nuriel <ronnuriel1@gmail.com>
2026-07-14 00:53:59 +0300

    discovery: add genome-wide mapping and gene ranking

--- src/viral_safe_target/__init__.py
+++ src/viral_safe_target/__init__.py
@@ -1,6 +1,6 @@
 """ViralSafeTarget computational research package."""
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from .annotations import annotate_candidates, read_gff3
 from .config import EditorProfile, load_config
